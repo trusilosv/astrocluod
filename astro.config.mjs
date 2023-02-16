54cc9fd143c1f4616eb7958ca257f51199da318a
Andrey Bulava <abulava@gmail.com>
2023-02-16 12:03:17 +0300

    Another way to get rid of trailing slashes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://cloudcannon-rnd.pages.dev",
+  build: {
+    format: 'file',
+  },
   trailingSlash: 'never',
   integrations: [react()]
 });
